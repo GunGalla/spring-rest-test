42779a74ef75e01b042d0309e6b442c7b9d99aa4
Iaroslav <GunGalla@gmail.com>
2023-08-09 15:16:08 +0300

    Add services

---
+++ src/main/java/com/gungalla/springresttask/service/TaskService.java
@@ -0,0 +1,19 @@
+package com.gungalla.springresttask.service;
+
+import com.gungalla.springresttask.domain.task.Task;
+
+import java.util.List;
+
+public interface TaskService {
+
+    Task getById(Long id);
+
+    List<Task> getAllByUserId(Long id);
+
+    Task update(Task task);
+
+    Task create(Task task);
+
+    void delete(Long id);
+
+}
