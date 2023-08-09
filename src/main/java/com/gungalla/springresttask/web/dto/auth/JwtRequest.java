e553738cac5d4e63c947ef697b421ae21edcf071
Iaroslav <GunGalla@gmail.com>
2023-08-09 16:06:42 +0300

    Add validations for JWT requests and users

--- src/main/java/com/gungalla/springresttask/web/dto/auth/JwtRequest.java
+++ src/main/java/com/gungalla/springresttask/web/dto/auth/JwtRequest.java
@@ -1,11 +1,15 @@
 package com.gungalla.springresttask.web.dto.auth;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class JwtRequest {
 
+    @NotNull(message = "Username must be not null.")
     private String username;
 
+    @NotNull(message = "Password must be not null.")
     private String password;
+
 }
--- src/main/java/com/gungalla/springresttask/web/dto/user/UserDto.java
+++ src/main/java/com/gungalla/springresttask/web/dto/user/UserDto.java
@@ -1,19 +1,32 @@
 package com.gungalla.springresttask.web.dto.user;
 
-import com.gungalla.springresttask.domain.task.Task;
-import com.gungalla.springresttask.domain.user.Role;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.gungalla.springresttask.web.dto.validation.OnCreate;
+import com.gungalla.springresttask.web.dto.validation.OnUpdate;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
-import java.util.List;
-import java.util.Set;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UserDto {
 
+    @NotNull(message = "Id must be not null.", groups = OnUpdate.class)
     private Long id;
+
+    @NotNull(message = "Name must be not null.", groups = {OnCreate.class, OnUpdate.class})
+    @Length(max = 255, message = "Name length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String name;
+
+    @NotNull(message = "Username must be not null.", groups = {OnCreate.class, OnUpdate.class})
+    @Length(max = 255, message = "Username length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull(message = "Password must be not null", groups = {OnCreate.class, OnUpdate.class})
     private String password;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull(message = "Password confirmation must be not null", groups = OnCreate.class)
     private String passwordConfirmation;
 
 }
