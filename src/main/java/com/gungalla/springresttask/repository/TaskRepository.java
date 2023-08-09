97f8a4c871c11db4c76238966318ad408b3621b4
Iaroslav <GunGalla@gmail.com>
2023-08-09 14:41:55 +0300

    Add repository for tasks

---
+++ src/main/java/com/gungalla/springresttask/repository/TaskRepository.java
@@ -0,0 +1,22 @@
+package com.gungalla.springresttask.repository;
+
+import com.gungalla.springresttask.domain.task.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository {
+
+    Optional<Task> findById(Long id);
+
+    List<Task> findAllByUserId(Long userId);
+
+    void assignToUserById(Long taskId, Long userId);
+
+    void update(Task task);
+
+    void create(Task task);
+
+    void delete(Long id);
+
+}
