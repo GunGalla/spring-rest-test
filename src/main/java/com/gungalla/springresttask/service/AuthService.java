3ea8e11d7a8b3342b6f3ec2579d225c9f52c0d22
Iaroslav <GunGalla@gmail.com>
2023-08-09 15:30:04 +0300

    Add auth JWT classes

--- src/main/java/com/gungalla/springresttask/service/AuthService.java
+++ src/main/java/com/gungalla/springresttask/service/AuthService.java
@@ -1,5 +1,9 @@
 package com.gungalla.springresttask.service;
 
+import com.gungalla.springresttask.web.dto.auth.JwtResponse;
+import com.gungalla.springresttask.web.dto.auth.JwtRequest;
+
+
 public interface AuthService {
 
     JwtResponse login(JwtRequest loginRequest);
---
+++ src/main/java/com/gungalla/springresttask/web/dto/auth/JwtRequest.java
@@ -0,0 +1,11 @@
+package com.gungalla.springresttask.web.dto.auth;
+
+import lombok.Data;
+
+@Data
+public class JwtRequest {
+
+    private String username;
+
+    private String password;
+}
