c5ad820c788b36318bf3bda912f8db1627056193
Iaroslav <GunGalla@gmail.com>
2023-08-16 13:56:07 +0300

    Implementation of Task and User services

--- src/main/java/com/gungalla/springresttask/service/impl/TaskServiceImpl.java
+++ src/main/java/com/gungalla/springresttask/service/impl/TaskServiceImpl.java
@@ -1,37 +1,58 @@
 package com.gungalla.springresttask.service.impl;
 
+import com.gungalla.springresttask.domain.exception.ResourceNotFoundException;
+import com.gungalla.springresttask.domain.task.Status;
 import com.gungalla.springresttask.domain.task.Task;
+import com.gungalla.springresttask.repository.TaskRepository;
 import com.gungalla.springresttask.service.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
+    private final TaskRepository taskRepository;
+
     @Override
+    @Transactional(readOnly = true)
     public Task getById(Long id) {
-        return null;
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found."));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Task> getAllByUserId(Long id) {
-        return null;
+        return taskRepository.findAllByUserId(id);
     }
 
     @Override
+    @Transactional
     public Task update(Task task) {
-        return null;
+        if (task.getStatus() == null) {
+            task.setStatus(Status.TODO);
+        }
+        taskRepository.update(task);
+        return task;
     }
 
     @Override
-    public Task create(Task task, Long id) {
-        return null;
+    @Transactional
+    public Task create(Task task, Long userId) {
+        task.setStatus(Status.TODO);
+        taskRepository.create(task);
+        taskRepository.assignToUserById(task.getId(), userId);
+        return task;
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
-
+        taskRepository.delete(id);
     }
 
 }
