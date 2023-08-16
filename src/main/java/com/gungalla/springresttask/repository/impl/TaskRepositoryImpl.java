3add42e2887ca94706818185bd2fb17dcdb6e444
Iaroslav <GunGalla@gmail.com>
2023-08-16 17:19:52 +0300

    Prepare statements for TaskRepository

--- src/main/java/com/gungalla/springresttask/repository/impl/TaskRepositoryImpl.java
+++ src/main/java/com/gungalla/springresttask/repository/impl/TaskRepositoryImpl.java
@@ -1,18 +1,76 @@
 package com.gungalla.springresttask.repository.impl;
 
+import com.gungalla.springresttask.domain.exception.ResourceMappingException;
 import com.gungalla.springresttask.domain.task.Task;
+import com.gungalla.springresttask.repository.DataSourceConfig;
 import com.gungalla.springresttask.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class TaskRepositoryImpl implements TaskRepository {
+
+    private final DataSourceConfig dataSourceConfig;
+
+    private final String FIND_BY_ID = """
+            SELECT t.id              as task_id,
+                   t.title           as task_title,
+                   t.description     as task_description,
+                   t.expiration_date as task_expiration_date,
+                   t.status          as task_status
+            FROM tasks t
+            WHERE id = ?""";
+
+    private final String FIND_ALL_BY_USER_ID = """
+            SELECT t.id              as task_id,
+                   t.title           as task_title,
+                   t.description     as task_description,
+                   t.expiration_date as task_expiration_date,
+                   t.status          as task_status
+            FROM tasks t
+                     JOIN users_tasks ut on t.id = ut.task_id
+            WHERE ut.user_id = ?""";
+
+    private final String ASSIGN = """
+            INSERT INTO users_tasks (task_id, user_id)
+            VALUES (?, ?)""";
+
+    private final String UPDATE = """
+            UPDATE tasks
+            SET title = ?,
+                description = ?,
+                expiration_date = ?,
+                status = ?
+            WHERE id = ?""";
+
+    private final String CREATE = """
+            INSERT INTO tasks (title, description, expiration_date, status)
+           VALUES (?, ?, ?, ?)""";
+
+    private final String DELETE = """
+            DELETE FROM tasks
+            WHERE id = ?""";
 
     @Override
     public Optional<Task> findById(Long id) {
-        return Optional.empty();
+        try {
+            Connection connection = dataSourceConfig.getConnection();
+            PreparedStatement statement = connection.prepareStatement(FIND_BY_ID);
+            statement.setLong(1, id);
+            try (ResultSet rs = statement.executeQuery()) {
+                return Optional.ofNullable(TaskRowMapper.mapRow(rs));
+            }
+        } catch (SQLException e) {
+            throw new ResourceMappingException("Cannot find user by id.");
+        }
     }
 
     @Override
