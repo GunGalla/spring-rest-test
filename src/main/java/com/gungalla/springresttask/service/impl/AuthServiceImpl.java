859c1c1259e678c24fae1f03808539be21d0f3ed
Iaroslav <GunGalla@gmail.com>
2023-08-16 11:36:53 +0300

    Implemented login method in Auth

--- src/main/java/com/gungalla/springresttask/service/impl/AuthServiceImpl.java
+++ src/main/java/com/gungalla/springresttask/service/impl/AuthServiceImpl.java
@@ -1,16 +1,34 @@
 package com.gungalla.springresttask.service.impl;
 
+import com.gungalla.springresttask.domain.user.User;
 import com.gungalla.springresttask.service.AuthService;
+import com.gungalla.springresttask.service.UserService;
 import com.gungalla.springresttask.web.dto.auth.JwtRequest;
 import com.gungalla.springresttask.web.dto.auth.JwtResponse;
+import com.gungalla.springresttask.web.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public JwtResponse login(JwtRequest loginRequest) {
-        return null;
+        JwtResponse jwtResponse = new JwtResponse();
+        authenticationManager.authenticate((new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())));
+        User user = userService.getByUsername(loginRequest.getUsername());
+        jwtResponse.setId(user.getId());
+        jwtResponse.setUsername(user.getUsername());
+        jwtResponse.setAccessToken(jwtTokenProvider.createAccessToken(user.getId(), user.getUsername(), user.getRoles()));
+        jwtResponse.setRefreshToken(jwtTokenProvider.createRefreshToken((user.getId()), user.getUsername()));
+        return jwtResponse;
     }
 
     @Override
