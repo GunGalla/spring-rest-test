8e938d535692f7dfe28f315b6dd9e8ff48a3678b
Iaroslav <GunGalla@gmail.com>
2023-08-22 15:26:26 +0300

    Add endpoints descriptions

--- src/main/java/com/gungalla/springresttask/web/controller/AuthController.java
+++ src/main/java/com/gungalla/springresttask/web/controller/AuthController.java
@@ -8,6 +8,8 @@ import com.gungalla.springresttask.web.dto.auth.JwtResponse;
 import com.gungalla.springresttask.web.dto.user.UserDto;
 import com.gungalla.springresttask.web.dto.validation.OnCreate;
 import com.gungalla.springresttask.web.mappers.UserMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "Auth Controller", description = "Auth API")
 public class AuthController {
 
     private final AuthService authService;
@@ -27,11 +30,13 @@ public class AuthController {
     private final UserMapper userMapper;
 
     @PostMapping("/login")
+    @Operation(summary = "Log in")
     public JwtResponse login(@Validated @RequestBody JwtRequest loginRequest) {
         return authService.login(loginRequest);
     }
 
     @PostMapping("/register")
+    @Operation(summary = "Register new user")
     public UserDto register(@Validated(OnCreate.class) @RequestBody UserDto userDto) {
         User user = userMapper.toEntity(userDto);
         User createdUser = userService.create(user);
@@ -39,6 +44,7 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
+    @Operation(summary = "Refresh JWT token")
     public JwtResponse refresh(@RequestBody String refreshToken) {
         return authService.refresh(refreshToken);
     }
