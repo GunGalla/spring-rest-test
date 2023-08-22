8a8cb36220563d43298c44e86a81db341a4d2644
Iaroslav <GunGalla@gmail.com>
2023-08-22 20:03:10 +0300

    Swagger configure finalize

--- src/main/java/com/gungalla/springresttask/web/dto/task/TaskDto.java
+++ src/main/java/com/gungalla/springresttask/web/dto/task/TaskDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gungalla.springresttask.domain.task.Status;
 import com.gungalla.springresttask.web.dto.validation.OnCreate;
 import com.gungalla.springresttask.web.dto.validation.OnUpdate;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -12,20 +13,26 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDateTime;
 
 @Data
+@Schema(description = "Task DTO")
 public class TaskDto {
 
+    @Schema(description = "Task id", example = "1")
     @NotNull(message = "Id must be not null.", groups = OnUpdate.class)
     private Long id;
 
+    @Schema(description = "Task title", example = "Create an app")
     @NotNull(message = "Title must be not null.", groups = {OnCreate.class, OnUpdate.class})
     @Length(max = 255, message = "Title length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String title;
 
+    @Schema(description = "Task description", example = "App must be able to manage tasks and use REST API")
     @Length(max = 255, message = "Description length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String description;
 
+    @Schema(description = "Task status", example = "TODO")
     private Status status;
 
+    @Schema(description = "Expiration date", example = "2023-08-25 12:20")
     @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime expirationDate;
--- src/main/java/com/gungalla/springresttask/web/dto/user/UserDto.java
+++ src/main/java/com/gungalla/springresttask/web/dto/user/UserDto.java
@@ -3,28 +3,35 @@ package com.gungalla.springresttask.web.dto.user;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gungalla.springresttask.web.dto.validation.OnCreate;
 import com.gungalla.springresttask.web.dto.validation.OnUpdate;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@Schema(description = "User DTO")
 public class UserDto {
 
+    @Schema(description = "User id", example = "1")
     @NotNull(message = "Id must be not null.", groups = OnUpdate.class)
     private Long id;
 
+    @Schema(description = "User name", example = "Jane Lake")
     @NotNull(message = "Name must be not null.", groups = {OnCreate.class, OnUpdate.class})
     @Length(max = 255, message = "Name length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String name;
 
+    @Schema(description = "User email", example = "janelake@gmail.com")
     @NotNull(message = "Username must be not null.", groups = {OnCreate.class, OnUpdate.class})
     @Length(max = 255, message = "Username length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String username;
 
+    @Schema(description = "User password", example = "qwerty")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull(message = "Password must be not null", groups = {OnCreate.class, OnUpdate.class})
     private String password;
 
+    @Schema(description = "User password confirmation", example = "qwerty")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull(message = "Password confirmation must be not null", groups = OnCreate.class)
     private String passwordConfirmation;
