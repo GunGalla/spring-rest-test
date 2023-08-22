a20407ff6d916eb167605d19de096a96874d72a8
Iaroslav <GunGalla@gmail.com>
2023-08-22 14:41:14 +0300

    Add swagger configuration

--- src/main/java/com/gungalla/springresttask/config/ApplicationConfig.java
+++ src/main/java/com/gungalla/springresttask/config/ApplicationConfig.java
@@ -2,6 +2,11 @@ package com.gungalla.springresttask.config;
 
 import com.gungalla.springresttask.web.security.JwtTokenFilter;
 import com.gungalla.springresttask.web.security.JwtTokenProvider;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,6 +41,26 @@ public class ApplicationConfig {
     }
 
     @Bean
+    public OpenAPI openAPI () {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info()
+                        .title("Task list API")
+                        .description("REST app for task management")
+                        .version("1.0")
+                );
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
@@ -54,6 +79,7 @@ public class ApplicationConfig {
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .anonymous(AbstractHttpConfigurer::disable)
                 .addFilterBefore(new JwtTokenFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
