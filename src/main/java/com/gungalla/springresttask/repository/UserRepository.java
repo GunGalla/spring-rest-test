1398cab280010515b32711745323bf0dc091acd4
Iaroslav <GunGalla@gmail.com>
2023-08-23 19:02:56 +0300

    Move to Spring Data JPA preparations, part 2 (prepare UserRepo)

--- src/main/java/com/gungalla/springresttask/repository/UserRepository.java
+++ src/main/java/com/gungalla/springresttask/repository/UserRepository.java
@@ -4,24 +4,14 @@ import com.gungalla.springresttask.domain.user.Role;
 import com.gungalla.springresttask.domain.user.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-@Mapper
-public interface UserRepository {
-
-    Optional<User> findById(Long id);
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
-    void update(User user);
-
-    void create(User user);
-
-    void insertUserRole(@Param("userId") Long userId, @Param("role") Role role);
-
     boolean isTaskOwner(@Param("userId") Long userId, @Param("taskId") Long taskId);
-
-    void delete(Long id);
 
 }
