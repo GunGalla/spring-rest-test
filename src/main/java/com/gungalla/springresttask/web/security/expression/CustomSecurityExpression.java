fa7e98c0b836590696f6b7ebbb077ad40558ce75
Iaroslav <GunGalla@gmail.com>
2023-08-23 11:33:10 +0300

    Add security expressions for auth checks

---
+++ src/main/java/com/gungalla/springresttask/web/security/expression/CustomSecurityExpression.java
@@ -0,0 +1,46 @@
+package com.gungalla.springresttask.web.security.expression;
+
+import com.gungalla.springresttask.domain.user.Role;
+import com.gungalla.springresttask.service.UserService;
+import com.gungalla.springresttask.web.security.JwtEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service("customSecurityExpression")
+@RequiredArgsConstructor
+public class CustomSecurityExpression {
+
+    private final UserService userService;
+
+    public boolean canAccessUser(Long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        JwtEntity user = (JwtEntity) authentication.getPrincipal();
+        Long userId = user.getId();
+
+        return userId.equals(id) || hasAnyRole(authentication, Role.ROLE_ADMIN);
+    }
+
+    private boolean hasAnyRole(Authentication authentication, Role... roles) {
+        for (Role role : roles) {
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.name());
+            if (authentication.getAuthorities().contains(authority)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean canAccessTask(Long task_id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        JwtEntity user = (JwtEntity) authentication.getPrincipal();
+        Long id = user.getId();
+
+        return userService.isTaskOwner(id, task_id);
+    }
+
+}
